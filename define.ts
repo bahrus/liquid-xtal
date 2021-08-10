bd2ffeef7dc6f9b1dca00d7b47b6df1360644a6f
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-08-10 07:55:01 -0400

    ...

---
+++ define.ts
@@ -0,0 +1,7 @@
+import { define as def } from 'xtal-element/lib/define.js';
+import { applyMixins } from 'xtal-element/lib/applyMixins.js';
+import { WCConfig } from './types.d.js';
+
+export function define(ctor:  {new(): Object}, mainTemplate: HTMLTemplateElement,  config: WCConfig[]){
+    
+}
---
+++ types.d.ts
@@ -0,0 +1,3 @@
+export interface WCConfig{
+    tagName?: string;
+}
